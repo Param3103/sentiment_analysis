267c1f722d2421d5d7d550032a4232671e5912f8
Param3103 <param.mehrotra@gmail.com>
2021-12-20 14:06:20 +0800

    have scrapped reviews from imdb

--- python scripts/web_scraping.py
+++ python scripts/web_scraping.py
@@ -10,8 +10,15 @@ urlopen(link1).close()
 urlopen(link2).close()
 page1_soup = soup(html1, "html.parser")
 page2_soup = soup(html2, "html.parser")
-reviews = page1_soup.findAll("div",{"class":"text show-more__control"})
-reviews2 = page2_soup.findAll("div",{"class":"text show-more__control"})
-reviews = [re.sub('<br/>','',str(review)[37:-6]) for review in reviews]+[re.sub('<br/>','',str(review)[37:-6]) for review in reviews2]
+page1_review = page1_soup.findAll("div",{"class":"text show-more__control"})
+page1_ratings = page1_soup.findAll("div", {"class":"ipl-ratings-bar"})
+page2_reviews = page2_soup.findAll("div",{"class":"text show-more__control"})
+page2_ratings = page2_soup.findAll("div",{"class":"ipl-ratings-bar"})
+# print(page1_ratings[0])
+page1_ratings = [int(str(rating.findAll("span")[-2])[6]) for rating in page1_ratings]
+page2_ratings = [int(str(rating.findAll("span")[-2])[6]) for rating in page2_ratings]
+reviews = [re.sub('<br/>','',str(review)[37:-6]) for review in page1_review]+[re.sub('<br/>','',str(review)[37:-6]) for review in page2_reviews]
+ratings = [1 if rating >= 5 else -1 for rating in page1_ratings+page2_ratings]
+
 # reviews has 50 reviews of 2 movies, upon which i will do natural language processing on
 
